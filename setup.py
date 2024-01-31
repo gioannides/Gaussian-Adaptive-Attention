51f8f810ab766795b09778ffe1a01df2b93e6ce3
gioannides <gioannides@hotmail.com>
2024-01-30 19:05:19 -0800

    Updated codebase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gaussian_adaptive_attention',
-    version='0.1.1',
+    version='0.1.0',
     author='Georgios Ioannides',
     author_email='gioannid@alumni.cmu.edu',
     description='A Gaussian Adaptive Attention module for PyTorch',
